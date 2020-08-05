b1a2b8b4aca5e50c9828b12570a6a92a6355a591
Jessica Benson <pokeyjess72@gmail.com>
2020-08-05 00:20:37 -0400

    Sets homepage as hello_app

--- hello_django/urls.py
+++ hello_django/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from hello_app.views import index
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
-    
 ]
